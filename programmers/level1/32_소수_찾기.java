8235daf70465c6a04de4f32857aa730dc0ad2864
lkhun9311 <96328441+lkhun9311@users.noreply.github.com>
2022-10-04 20:21:09 +0900

    Update 32_소수_찾기.java

--- programmers/level1/32_소수_찾기.java
+++ programmers/level1/32_소수_찾기.java
@@ -33,3 +33,40 @@ class Main {
         System.out.println(method.main(n));
     }
 }
+
+//소수 찾기(방법 2. 에라토스테네스의 체)
+// Math.sqrt(n)보다 작거나 같은 소수둘의 배수는 모두 소수가 아니다.
+//class Main {
+//    public int main(int n) {
+//        int answer = 0;
+//        List<Integer> arr = new ArrayList<>(Arrays.asList(0, 0));
+//
+//        for(int i = 2; i <= n; i++) {
+//            arr.add(i);
+//        }
+//
+//        for(int element : filter(n, arr)) {
+//            if(element != 0) answer++;
+//        }
+//
+//        return answer;
+//    }
+//
+//    public List<Integer> filter(int n, List<Integer> arr) {
+//
+//        for(int i = 2; i <= Math.sqrt(n); i++) {
+//            if(arr.get(i) == 0) continue;
+//            for(int j = 2*i; j <= n; j += i) {
+//                arr.set(j, 0);
+//            }
+//        }
+//
+//        return arr;
+//    }
+//
+//    public static void main(String[] args) {
+//        Main method = new Main();
+//        int n = 10;
+//        System.out.println(method.main(n));
+//    }
+//}
